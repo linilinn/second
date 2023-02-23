5e50fb6f88bc58a9bfd7c0671aeb9cb42ed1e725
meow <meowmeow@gmail.com>
2023-02-23 21:19:01 +0300

    third task

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.second;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button); //кнопка Далее
+        button.setText(R.string.next);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName); //поле ввода имени
+        editText.setText(R.string.user_name);
+        TextView appTextView = (TextView) findViewById(R.id.textView); //Mobile Waiter
+        appTextView.setText(R.string.app_name);
+        TextView restaurant_appTextView = (TextView) findViewById(R.id.textView2); //Приложение для ресторанов
+        restaurant_appTextView.setText(R.string.app_for);
+        TextView usernameTextView = (TextView) findViewById(R.id.textView3); //Как к вам можно обращаться?
+        usernameTextView.setText(R.string.start_screen);
+        ImageView myImage = (ImageView) findViewById(R.id.imageView3); //картинка еды на стартовом экране
+        myImage.setImageResource(R.drawable.start_food);
     }
 }

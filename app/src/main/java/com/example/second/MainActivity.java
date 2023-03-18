89e34b9705c3b1334dd2a0faadab212ab8c072be
meow <meowmeow@gmail.com>
2023-03-18 14:00:36 +0300

    fixed version

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -21,13 +21,13 @@ import android.widget.Toast;
 import com.example.second.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "First Activity";
-    private ActivityMainBinding binding;
+    //private ActivityMainBinding binding;
     Context context = MainActivity.this;
-    int REQUEST_CODE = 1;
     int time = Toast.LENGTH_LONG; //длительность всплывающего окна
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button); //кнопка Далее
         button.setText(R.string.next);
@@ -48,34 +48,20 @@ public class MainActivity extends AppCompatActivity {
                 EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
                 String name = editText.getText().toString();
                 intent.putExtra("name", name);
-                startActivityForResult(intent, 1);
+                mStartForResult.launch(intent);
             }
         });
+    }
 
-        /*
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        binding.button.setText(R.string.next);
-        binding.editTextTextPersonName.setText(R.string.user_name);
-        binding.textView.setText(R.string.app_name);
-        binding.textView2.setText(R.string.app_for);
-        binding.textView3.setText(R.string.start_screen);
-        binding.imageView3.setImageResource(R.drawable.start_food);
-        binding.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "Button clicked");
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent intent = result.getData();
+                    String login = intent.getStringExtra("login");
+                    Toast.makeText(context, login + ", приложение пока не готово", time).show();
+                }
             }
-        });
-        */
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
-            String resultText = data.getStringExtra("resultText");
-            Toast.makeText(context, resultText + ", приложение пока не готово", time).show();
-        }
-    }
+    );
 }
--- app/src/main/java/com/example/second/SecondActivity.java
+++ app/src/main/java/com/example/second/SecondActivity.java
@@ -11,33 +11,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.second.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
-    Context context2 = SecondActivity.this;
-    private ActivitySecondBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Bundle arguments = getIntent().getExtras();
-        initIt();
-    }
-    private void initIt() {
-        Bundle argument = getIntent().getExtras();
-        String name = argument.get("name").toString();
+        String name = arguments.get("name").toString();
         TextView textView8 = (TextView) findViewById(R.id.textView8);
         textView8.setText("Здравствуйте, " + name + "!");
-        Button button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener( view -> {
-            Intent intent = new Intent();
-            EditText loginEditText = (EditText) findViewById(R.id.editTextLogin); //поле ввода логина
-            String login = loginEditText.getText().toString();
-            intent.putExtra("resultText", login);
-            setResult(Activity.RESULT_OK, intent);
-            finish();
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                EditText loginEditText = (EditText) findViewById(R.id.editTextLogin); //поле ввода логина
+                String login = loginEditText.getText().toString();
+                intent.putExtra("login", login);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
         });
     }
 }
